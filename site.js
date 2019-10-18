3a403222e282e4edf9fb760c9712be295a3799f8
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-10-18 16:45:33 -0400

    fixed?

--- site.js
+++ site.js
@@ -1,10 +1,10 @@
 ﻿var honkCount = 0;
 
-var honk1 = new Audio('sounds/sfx_goose_honk_01.mp3');
-var honk2 = new Audio('sounds/sfx_goose_honk_02.mp3');
-var honk3 = new Audio('sounds/sfx_goose_honk_03.mp3');
-var honk4 = new Audio('sounds/sfx_goose_honk_04.mp3');
-var honk5 = new Audio('sounds/sfx_goose_honk_05.mp3');
+var honk1 = new Audio('sfx_goose_honk_01.mp3');
+var honk2 = new Audio('sfx_goose_honk_02.mp3');
+var honk3 = new Audio('sfx_goose_honk_03.mp3');
+var honk4 = new Audio('sfx_goose_honk_04.mp3');
+var honk5 = new Audio('sfx_goose_honk_05.mp3');
 
 var honks = Array(honk1,honk2,honk3,honk4,honk5);
 
@@ -17,7 +17,7 @@ function doHonk(e) {
         honk.play(); 
         honkCount++;
         document.getElementById("count").innerHTML = honkCount;
-        if (honkCount >= 50){
+        if (honkCount >= 15){
             document.getElementById("monster").style = "display:visible; font-size:50pt;";
         }
 
