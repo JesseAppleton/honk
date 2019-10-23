399541add5ce09145883e331e1bad9ee9c1e5a58
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-10-23 17:54:57 -0400

    added mobile

--- site.js
+++ site.js
@@ -1,4 +1,5 @@
 ﻿var honkCount = 0;
+var opactiyCount = 0;
 
 var honk1 = new Audio('sfx_goose_honk_01.mp3');
 var honk2 = new Audio('sfx_goose_honk_02.mp3');
@@ -8,19 +9,29 @@ var honk5 = new Audio('sfx_goose_honk_05.mp3');
 
 var honks = Array(honk1,honk2,honk3,honk4,honk5);
 
-window.addEventListener("keydown", doHonk);
+window.addEventListener("keydown", honked);
 
-function doHonk(e) {
+function honked(e) {
     var keyCode = (e.keyCode ? e.keyCode : e.which);
-    var honk = honks[Math.floor(Math.random()*honks.length)];
-    if (keyCode === 89) { 
-        honk.play(); 
-        honkCount++;
-        document.getElementById("count").innerHTML = honkCount;
-        if (honkCount >= 15){
-            document.getElementById("monster").style = "display:visible; font-size:50pt;";
-        }
+    if (keyCode === 89) {
+        doHonk();
+    }
+}
 
-        //fade in image of goose based on times honked?
+function doHonk() {
+    var honk = honks[Math.floor(Math.random()*honks.length)];
+    honk.play(); 
+    honkCount++;
+    document.getElementById("count").innerHTML = honkCount;
+
+    if (honkCount >= 15) {
+        document.getElementById("monster").style = "display:visible; font-size:50pt;";
+    }
+
+    // fade in image of goose per honk
+    if (opactiyCount < 1.0) {
+        // console.log("Changing opacity: " + opactiyCount);
+        opactiyCount += 0.05;
+        document.getElementById("backer").style = "opacity:"+opactiyCount+";";
     }
 }
